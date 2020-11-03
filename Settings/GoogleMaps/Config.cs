15d09368c9fe12e5e781ad01eafeae6ac7697546
James <31246882+Jimud1@users.noreply.github.com>
2020-11-03 14:30:22 +0000

    Update Config.cs

--- Settings/GoogleMaps/Config.cs
+++ Settings/GoogleMaps/Config.cs
@@ -2,6 +2,6 @@
 {
     public static class Config
     {
-        public static string GoogleMapsApiKey => "AIzaSyAXYkW0rqkFibxqhNSgCeEt7gYQHjtt2DA";
+        public static string GoogleMapsApiKey => "ghfg";
     }
 }
